a6943768e672b0eff4a1252079ace66853cd74d2
Edward A Maxedon Sr <edadma@gmail.com>
2022-01-18 15:47:44 -0500

    code calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,49 @@
-import React from 'react'
-import {
-  Button,
-  Card,
-  Grid,
-  ThemeProvider,Text
-} from '@edadma/react-tailwind'
+import React, { useState } from 'react'
+import { Button, Card, Grid, ThemeProvider, Text, Elem } from '@edadma/react-tailwind'
 import { DefaultTheme } from '@edadma/react-tailwind/dist/themes/DefaultTheme'
 
+interface State {
+  display: string
+  op: string | null
+}
+
 function App() {
+  const [state, setState] = useState<State>({ display: '0', op: null })
+
+  const Key: React.FC = ({ children }) => <Button rounded={false}>{children}</Button>
   return (
     <ThemeProvider value={DefaultTheme}>
-      <Card className="w-3/12">
+      <Card bg="primary" className="w-3/12">
         <Grid cols={1}>
-          <Button>asdf</Button>
-          <Grid cols={5} bg="primary">
-            <Button>regular</Button>
-            <Button role="info">info</Button>
-            <Button role="success">success</Button>
-            <Button role="warning">warning</Button>
-            <Button role="error">error</Button>
-            <Button outlined>regular</Button>
-            <Button outlined role="info">
-              info
-            </Button>
-            <Button outlined role="success">
-              success
-            </Button>
-            <Button outlined role="warning">
-              warning
-            </Button>
-            <Button outlined role="error">
-              error
-            </Button>
+          <Card pad={2} className="flex bg-gray-800">
+            <Text size="lg" className="flex-1 text-white" align="right">
+              {state.display}
+            </Text>
+          </Card>
+          <Grid cols={6}>
+            <Key>7</Key>
+            <Key>8</Key>
+            <Key>9</Key>
+            <Key>÷</Key>
+            <Key>⤺</Key>
+            <Key>4</Key>
+            <Key>5</Key>
+            <Key>6</Key>
+            <Key>×</Key>
+            <Key>C</Key>
+            <Key>1</Key>
+            <Key>2</Key>
+            <Key>3</Key>
+            <Key>-</Key>
+            <Key>0</Key>
+            <Key>.</Key>
+            <Key>%</Key>
+            <Key>+</Key>
+            <Elem rowSpan="2" className="flex">
+              <Button className="flex-1" role="primary" rounded={false}>
+                =
+              </Button>
+            </Elem>
           </Grid>
         </Grid>
       </Card>
