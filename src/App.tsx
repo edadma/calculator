bc0531f520802260a61e5e5197f03ed64d307509
Edward A Maxedon Sr <edadma@gmail.com>
2022-01-18 18:39:47 -0500

    code calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { MouseEventHandler, useReducer, useState } from 'react'
 import { Button, Card, Grid, ThemeProvider, Text, Elem } from '@edadma/react-tailwind'
 import { DefaultTheme } from '@edadma/react-tailwind/dist/themes/DefaultTheme'
 
@@ -7,45 +7,94 @@ interface State {
   op: string | null
 }
 
-function App() {
-  const [state, setState] = useState<State>({ display: '0', op: null })
+type Action = { type: 'digit'; digit: string } | { type: 'function'; func: string }
 
-  const Key: React.FC = ({ children }) => <Button rounded={false}>{children}</Button>
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'digit':
+      return {
+        display: state.display === '0' ? action.digit : state.display + action.digit,
+        op: state.op,
+      }
+    case 'function':
+      switch (action.func) {
+        case 'C':
+          return { display: '0', op: null }
+        case '⤺':
+          return {
+            display:
+              state.display.length === 1
+                ? '0'
+                : state.display.substring(0, state.display.length - 1),
+            op: state.op,
+          }
+        case '.':
+          return state.display.includes('.')
+            ? state
+            : { display: state.display + '.', op: state.op }
+        default:
+          throw new Error('Invalid')
+      }
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { display: '0', op: null })
+
+  const handleDigitKeyClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    dispatch({ type: 'digit', digit: (e.target as HTMLButtonElement).firstChild?.nodeValue! })
+  }
+
+  const handleFunctionKeyClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    dispatch({ type: 'function', func: (e.target as HTMLButtonElement).firstChild?.nodeValue! })
+  }
+
+  const DigitKey: React.FC = ({ children }) => (
+    <Button icon rounded={false} onClick={handleDigitKeyClick}>
+      {children}
+    </Button>
+  )
+
+  const FunctionKey: React.FC = ({ children }) => (
+    <Button icon rounded={false} onClick={handleFunctionKeyClick}>
+      {children}
+    </Button>
+  )
 
   return (
     <ThemeProvider value={DefaultTheme}>
-      <Card bg="primary" className="w-3/12">
-        <Grid cols={1}>
-          <Card pad={2} className="flex bg-gray-800">
-            <Text size="lg" className="flex-1 text-white" align="right">
-              {state.display}
-            </Text>
-          </Card>
-          <Grid cols={6}>
-            <Key>7</Key>
-            <Key>8</Key>
-            <Key>9</Key>
-            <Key>÷</Key>
-            <Key>⤺</Key>
-            <Key>4</Key>
-            <Key>5</Key>
-            <Key>6</Key>
-            <Key>×</Key>
-            <Key>C</Key>
-            <Key>1</Key>
-            <Key>2</Key>
-            <Key>3</Key>
-            <Key>-</Key>
-            <Key>0</Key>
-            <Key>.</Key>
-            <Key>%</Key>
-            <Key>+</Key>
-            <Elem rowSpan="2" className="flex">
-              <Button className="flex-1" role="primary" rounded={false}>
-                =
-              </Button>
-            </Elem>
-          </Grid>
+      <Card pad={4} bg="primary" className="w-80">
+        <Grid cols={5}>
+          <Elem colSpan="5">
+            <div className="flex bg-gray-800 p-2">
+              <span className="flex-1 text-white text-xl font-bold font-mono text-right">
+                {state.display}
+              </span>
+            </div>
+          </Elem>
+          <DigitKey>7</DigitKey>
+          <DigitKey>8</DigitKey>
+          <DigitKey>9</DigitKey>
+          <FunctionKey>÷</FunctionKey>
+          <FunctionKey>⤺</FunctionKey>
+          <DigitKey>4</DigitKey>
+          <DigitKey>5</DigitKey>
+          <DigitKey>6</DigitKey>
+          <FunctionKey>×</FunctionKey>
+          <FunctionKey>C</FunctionKey>
+          <DigitKey>1</DigitKey>
+          <DigitKey>2</DigitKey>
+          <DigitKey>3</DigitKey>
+          <FunctionKey>-</FunctionKey>
+          <Elem rowSpan="2" className="flex">
+            <Button className="flex-1" role="primary" rounded={false}>
+              =
+            </Button>
+          </Elem>
+          <DigitKey>0</DigitKey>
+          <FunctionKey>.</FunctionKey>
+          <FunctionKey>±</FunctionKey>
+          <FunctionKey>+</FunctionKey>
         </Grid>
       </Card>
     </ThemeProvider>
